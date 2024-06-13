579256af979c73a6643ccc2633c215e7ad8397e7
VakeDomen <vake.domen94@gmail.com>
2024-06-13 18:51:28 +0200

    updated

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,18 +1,15 @@
-import { Telegraf } from 'telegraf';
-import * as dotenv from 'dotenv';
-import getUrls = require('get-urls');
-import * as fetch from 'node-fetch';
-import cheerio = require('cheerio');
-const fs = require('fs');
+import { readFileSync, writeFileSync } from "fs";
+const { Telegraf } = require("telegraf");
+import puppeteer from 'puppeteer';
 
-let bot: Telegraf;
+let bot: any;
 let deals: any = {}
 let timer = false;
 let last = new Date();
+let url = null;
 
 function checkEnv(): void {
     console.log("Checking credentials...");
-    dotenv.config();
     if (!process.env.BOT_TOKEN) {
         console.log("Bot token not specified!");
         process.exit(1);
@@ -26,79 +23,139 @@ function start(): void {
     console.log("Launching bot...");
     bot.launch();
     console.log("Done!");
-    const links = fs.readFileSync('data.txt', {encoding:'utf8', flag:'r'}).split(',');
+    const links = readFileSync('data.txt', { encoding: 'utf8', flag: 'r' }).split(',') ?? [];
     const ids = links.map((link: string) => link.substr(40, 8));
-    console.log(links, ids)
-    for (let i = 0 ; i < links.length ; i++) {
+    for (let i = 0; i < links.length; i++) {
         if (!deals[ids[i]]) {
             deals[ids[i]] = links[i];
         }
     }
-    console.log(deals)
 }
 
 async function scrape(ctx, silent?: boolean) {
     console.log('scpraping')
-    
-    const url = Array.from(
-        getUrls('https://www.avto.net/Ads/results.asp?znamka=BMW&model=M2&modelID=&tip=katerikoli%20tip&znamka2=&model2=&tip2=katerikoli%20tip&znamka3=&model3=&tip3=katerikoli%20tip&cenaMin=100&cenaMax=100000&letnikMin=2006&letnikMax=2017&bencin=201&starost2=999&oblika=0&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=150000&kwMin=0&kwMax=999&motortakt=&motorvalji=&lokacija=0&sirina=&dolzina=&dolzinaMIN=&dolzinaMAX=&nosilnostMIN=&nosilnostMAX=&lezisc=&presek=&premer=&col=&vijakov=&EToznaka=&vozilo=&airbag=&barva=&barvaint=&doseg=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000000000&EQ7=1000000120&EQ8=101000000&EQ9=1000000020&KAT=1010000000&PIA=&PIAzero=&PIAOut=&PSLO=&akcija=&paketgarancije=&broker=&prikazkategorije=&kategorija=&ONLvid=&ONLnak=&zaloga=&arhiv=&presort=&tipsort=&stran=')
-    ).pop();
-    let options: any = {};
-    options.redirect = "follow";
-    options.follow = 20;
-    options.headers = {
-        "method":"GET",
-        "path":"/catalog/classlist?k=math&t=2177&e=all&hon=F&promod=F",
-        "scheme":"https",
-        "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "accept-encoding":"gzip, deflate, sdch, br",
-        "accept-language":"en-US,en;q=0.8",
-        "cache-control":"no-cache",
-        "cookie":"JSESSIONID=javaprod19~413DF4150236B1466C8ECB85EB796C06.catalog19; onlineCampusSelection=C; __cfduid=d5e9cb96f2485f7500fec2116ee8f23381491087061; __utma=59190898.1874896314.1491088625.1491088625.1491088625.1; __utmb=59190898.2.10.1491088625; __utmc=59190898; __utmz=59190898.1491088625.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=137925942.2000995260.1491087063.1491087063.1491088718.2; __utmb=137925942.2.10.1491088718; __utmc=137925942; __utmz=137925942.1491088718.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); ADRUM=s=1491089349546&r=https%3A%2F%2Fwebapp4.asu.edu%2Fcatalog%2Fclasslist%3F-1275642430",
-        "pragma":"no-cache",
-        "referer":"https://webapp4.asu.edu/catalog/",
-        "upgrade-insecure-requests":"1",
-        "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
-    }
-                
-    const resp = await fetch(url, options);
-    const html = await resp.text();
-    const $ = cheerio.load(html);
-    // console.log(html)
-    const res = $('.stretched-link').splice(8);
-    const links = res
-        .map((el: any) => el['attribs']['href'])
-        .map((link: string) => `https://www.avto.net${link.substring(2)}`);
 
-    const ids = links.map((link: string) => link.substr(40, 8));
-    let found = false;
-    for (let i = 0 ; i < links.length ; i++) {
-        if (!deals[ids[i]]) {
-            deals[ids[i]] = links[i];
-            ctx.reply(deals[ids[i]])
-            found = true;
+    if (!url) {
+        ctx.reply("No filter url set. Provide a filter url with /setfilter <https://avto.net/Ads...>")
+        return
+    }
+
+
+    (async () => {
+        // Launch Puppeteer in headless mode
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        // Set custom headers
+        await page.setExtraHTTPHeaders({
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "accept-encoding": "gzip, deflate, sdch, br",
+            "accept-language": "en-US,en;q=0.8",
+            "cache-control": "no-cache",
+            "cookie": "JSESSIONID=javaprod19~413DF4150236B1466C8ECB85EB796C06.catalog19; onlineCampusSelection=C; __cfduid=d5e9cb96f2485f7500fec2116ee8f23381491087061; __utma=59190898.1874896314.1491088625.1491088625.1491088625.1; __utmb=59190898.2.10.1491088625; __utmc=59190898; __utmz=59190898.1491088625.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=137925942.2000995260.1491087063.1491087063.1491088718.2; __utmb=137925942.2.10.1491088718; __utmc=137925942; __utmz=137925942.1491088718.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); ADRUM=s=1491089349546&r=https%3A%2F%2Fwebapp4.asu.edu%2Fcatalog%2Fclasslist%3F-1275642430",
+            "pragma": "no-cache",
+            "referer": "https://webapp4.asu.edu/catalog/",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+        });
+
+        // Navigate to the URL
+        await page.goto(url, {
+            waitUntil: 'networkidle2'
+        });
+
+        // Wait for the challenge to complete by checking for an element that appears only after the challenge
+        await page.waitForSelector('.stretched-link', { timeout: 60000 });
+
+        // Get the page content
+        const html = await page.content();
+
+        // Use Cheerio to parse the HTML
+        const cheerio = require('cheerio');
+        const $ = cheerio.load(html);
+
+        // Example: Print out the page title
+        console.log(html)
+        const res = $('.stretched-link').splice(8);
+        const links = res
+            .map((el: any) => el['attribs']['href'])
+            .map((link: string) => `https://www.avto.net${link.substring(2)}`);
+
+        const ids = links.map((link: string) => link.substr(40, 8));
+        let found = false;
+        for (let i = 0; i < links.length; i++) {
+            if (!deals[ids[i]]) {
+                deals[ids[i]] = links[i];
+                ctx.reply(`New offer: ${deals[ids[i]]}`)
+                found = true;
+            }
         }
-    }
-    console.log(silent)
-    if (!found && !silent) {
-        ctx.reply('No new deals. Type /all to see all found deals')
-    }
-    fs.writeFileSync('data.txt', links.toString());
-    last = new Date();
+        console.log(silent)
+        if (!found && !silent) {
+            ctx.reply('No new deals. Type /all to see all found deals')
+        }
+        writeFileSync('data.txt', links.toString());
+        last = new Date();
+        // Close Puppeteer
+        await browser.close();
+    })();
+
+
 }
 
 function initBot(): void {
     console.log("Setting up bot...");
     bot = new Telegraf(process.env.BOT_TOKEN || '');
     bot.command('scrape', (ctx) => scrape(ctx));
-    bot.command('all', allDeals);
-    bot.command('timer', setTimer);
-    bot.command('stop', unsetTimer);
-    bot.command('last', lastScrape);
+    bot.command('all', (ctx) => allDeals(ctx));
+    bot.command('start', (ctx) => setTimer(ctx));
+    bot.command('stop', (ctx) => unsetTimer(ctx));
+    bot.command('last', (ctx) => lastScrape(ctx));
+    bot.command('status', (ctx) => status(ctx))
+    bot.command('setfilter', (ctx) => setFilter(ctx))
+    bot.command('getfilter', (ctx) => getFilter(ctx))
+    bot.command('help', (ctx) => help(ctx))
+
     console.log("Bot set up!");
 }
 
+function help(ctx) {
+
+    ctx.reply(`
+/scrape - manually scrape current filter
+/status - status of the bot
+/all - all found deals (careful...spamm)
+/start - start the automated scrape timer
+/stop - stop the automated scrape timer
+/setfilter - Provide a filter url with /setfilter <https://avto.net/Ads...>
+/last - time of the last scrape
+    `)
+}
+
+function setFilter(ctx) {
+    try {
+        url = ctx["update"]["message"]["text"].split(" ")[1];
+        if (!url) {
+            ctx.reply("Couldn't parse url filter. Provide a filter url with /setfilter <https://avto.net/Ads...>")
+        }
+    } catch (e) {
+        ctx.reply("Something went wrong... make sure you only paste the link.")
+    }
+}
+
+
+function getFilter(ctx) {
+    ctx.reply("Current filter: \n" + url);
+}
+
 async function setTimer(ctx) {
+    if (timer) {
+        ctx.reply(`Already running`)
+        return
+    }
     timer = true;
     loop(ctx)
 }
@@ -110,24 +167,38 @@ function lastScrape(ctx): void {
 function loop(ctx) {
     if (timer) {
         scrape(ctx, true);
-        last
         setTimeout(() => loop(ctx), 600000)
     }
 }
 
 function unsetTimer(ctx) {
+    if (!timer) {
+        ctx.reply(`Not running...`)
+        return
+    }
     timer = false;
     ctx.reply('Scraping timer stopped!')
 }
 
+function status(ctx) {
+    let resp = "";
+    if (timer) resp += `🔴 Not running...`;
+    else resp += "🟢 Running...";
+    resp += "\n";
+    resp += `Last scrape: ${last.toString()}`
+    ctx.reply(resp)
+}
+
 function allDeals(ctx) {
+    if (!deals) {
+        ctx.reply("Sorry...no offers yet..")
+        return
+    }
+    ctx.reply("Current found deals:")
     for (const deal in deals) {
+
         ctx.reply(deals[deal]);
     }
 }
 
-
-function sleep(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-}
 start();
