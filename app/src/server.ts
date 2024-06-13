0c4d50f57df81dd7bca74a6bc65aa3dbafc9e503
VakeDomen <vake.domen94@gmail.com>
2024-06-13 19:04:13 +0200

    double scraping protection

--- app/src/server.ts
+++ app/src/server.ts
@@ -5,6 +5,7 @@ import puppeteer from 'puppeteer';
 let bot: any;
 let deals: any = {}
 let timer = false;
+let scraping = false;
 let last = new Date();
 let url = null;
 
@@ -39,68 +40,81 @@ async function scrape(ctx, silent?: boolean) {
         ctx.reply("No filter url set. Provide a filter url with /setfilter <https://avto.net/Ads...>")
         return
     }
+    if (scraping) {
+        ctx.reply("Currently scraping...wait a minute...")
+        return
+    }
 
 
     (async () => {
-        // Launch Puppeteer in headless mode
-        const browser = await puppeteer.launch({
-            headless: true,
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
-        // Set custom headers
-        await page.setExtraHTTPHeaders({
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "accept-encoding": "gzip, deflate, sdch, br",
-            "accept-language": "en-US,en;q=0.8",
-            "cache-control": "no-cache",
-            "cookie": "JSESSIONID=javaprod19~413DF4150236B1466C8ECB85EB796C06.catalog19; onlineCampusSelection=C; __cfduid=d5e9cb96f2485f7500fec2116ee8f23381491087061; __utma=59190898.1874896314.1491088625.1491088625.1491088625.1; __utmb=59190898.2.10.1491088625; __utmc=59190898; __utmz=59190898.1491088625.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=137925942.2000995260.1491087063.1491087063.1491088718.2; __utmb=137925942.2.10.1491088718; __utmc=137925942; __utmz=137925942.1491088718.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); ADRUM=s=1491089349546&r=https%3A%2F%2Fwebapp4.asu.edu%2Fcatalog%2Fclasslist%3F-1275642430",
-            "pragma": "no-cache",
-            "referer": "https://webapp4.asu.edu/catalog/",
-            "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
-        });
+        scraping = true;
+        try {
 
-        // Navigate to the URL
-        await page.goto(url, {
-            waitUntil: 'networkidle2'
-        });
-
-        // Wait for the challenge to complete by checking for an element that appears only after the challenge
-        await page.waitForSelector('.stretched-link', { timeout: 60000 });
-
-        // Get the page content
-        const html = await page.content();
-
-        // Use Cheerio to parse the HTML
-        const cheerio = require('cheerio');
-        const $ = cheerio.load(html);
-
-        // Example: Print out the page title
-        console.log(html)
-        const res = $('.stretched-link').splice(8);
-        const links = res
-            .map((el: any) => el['attribs']['href'])
-            .map((link: string) => `https://www.avto.net${link.substring(2)}`);
-
-        const ids = links.map((link: string) => link.substr(40, 8));
-        let found = false;
-        for (let i = 0; i < links.length; i++) {
-            if (!deals[ids[i]]) {
-                deals[ids[i]] = links[i];
-                ctx.reply(`New offer: ${deals[ids[i]]}`)
-                found = true;
+            // Launch Puppeteer in headless mode
+            const browser = await puppeteer.launch({
+                headless: true,
+                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+            const page = await browser.newPage();
+            // Set custom headers
+            await page.setExtraHTTPHeaders({
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "accept-encoding": "gzip, deflate, sdch, br",
+                "accept-language": "en-US,en;q=0.8",
+                "cache-control": "no-cache",
+                "cookie": "JSESSIONID=javaprod19~413DF4150236B1466C8ECB85EB796C06.catalog19; onlineCampusSelection=C; __cfduid=d5e9cb96f2485f7500fec2116ee8f23381491087061; __utma=59190898.1874896314.1491088625.1491088625.1491088625.1; __utmb=59190898.2.10.1491088625; __utmc=59190898; __utmz=59190898.1491088625.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utma=137925942.2000995260.1491087063.1491087063.1491088718.2; __utmb=137925942.2.10.1491088718; __utmc=137925942; __utmz=137925942.1491088718.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); ADRUM=s=1491089349546&r=https%3A%2F%2Fwebapp4.asu.edu%2Fcatalog%2Fclasslist%3F-1275642430",
+                "pragma": "no-cache",
+                "referer": "https://webapp4.asu.edu/catalog/",
+                "upgrade-insecure-requests": "1",
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+            });
+    
+            // Navigate to the URL
+            await page.goto(url, {
+                waitUntil: 'networkidle2'
+            });
+    
+            // Wait for the challenge to complete by checking for an element that appears only after the challenge
+            await page.waitForSelector('.stretched-link', { timeout: 60000 });
+    
+            // Get the page content
+            const html = await page.content();
+    
+            // Use Cheerio to parse the HTML
+            const cheerio = require('cheerio');
+            const $ = cheerio.load(html);
+    
+            // Example: Print out the page title
+            console.log(html)
+            const res = $('.stretched-link').splice(8);
+            const links = res
+                .map((el: any) => el['attribs']['href'])
+                .map((link: string) => `https://www.avto.net${link.substring(2)}`);
+    
+            const ids = links.map((link: string) => link.substr(40, 8));
+            let found = false;
+            for (let i = 0; i < links.length; i++) {
+                if (!deals[ids[i]]) {
+                    deals[ids[i]] = links[i];
+                    ctx.reply(`New offer: ${deals[ids[i]]}`)
+                    found = true;
+                }
             }
+            console.log(silent)
+            if (!found && !silent) {
+                ctx.reply('No new deals. Type /all to see all found deals')
+            }
+            writeFileSync('data.txt', links.toString());
+            last = new Date();
+            // Close Puppeteer
+            await browser.close();
+            scraping = false
+        } catch (e) {
+            ctx.reply("Something went wrong scraping...check server logs")
+            console.error(e)
+            scraping = false
         }
-        console.log(silent)
-        if (!found && !silent) {
-            ctx.reply('No new deals. Type /all to see all found deals')
-        }
-        writeFileSync('data.txt', links.toString());
-        last = new Date();
-        // Close Puppeteer
-        await browser.close();
     })();
 
 
